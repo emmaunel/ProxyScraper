2a386b556115efadffb1753de1147a9dba23f6c3
Emmanuel <aadewale123@gmail.com>
2020-03-15 22:35:29 -0400

    Added Location, update command line flags, & testing proxy status

--- README.md
+++ README.md
@@ -4,12 +4,18 @@ A fast & simple proxy scraper in golang
 ## Features
 - Scarpes for proxies, obvisiouly
 - checks proxies speeds(speed depends on your network connections)
+- Tells you the location of the proxy
+
+## Installation
 
 ## TODO
 - [x] Find proxies
 - [ ] test speeds
 - [ ] Chain proxies together based on the second bullet point
+- [ ] Encrypt traffic???
+- [ ] Look into socks proxies
 
 
 ## Bugs
 - [ ] Fixed dulipcate proxies in the outfile
+- [ ] Scraper finishes before location if received. (threading problem, i think)
--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"bufio"
 	"time"
 	"github.com/akamensky/argparse"
+	"strings"
+	"encoding/json"
 	)
 
 var red = "\033[1;31m"
@@ -19,6 +21,45 @@ var defcol = "\033[0m"
 
 var re = regexp.MustCompile(`[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}(\:[\d]{2,5})?`)
 
+
+// TODO: option for stdout
+// var stdout = true
+// if outfile is specified, this will become false
+
+/**
+* Location Struct to parse the proxy location from json 
+*/
+type Location struct {
+	// IP string `json:"ip"`
+	// City string `json:"city"`
+	// Region string `json:"region"`
+	// Region_code string `json:"region_code"`
+	// Country string `json:"country"`
+	// Country_code string `json:"country_code"`
+	// Country_code_iso3 string `json:"country_code_iso3"`
+	// Country_capital string `json:"country_capital"`
+	// Country_tld string `json:"country_tld"`
+	Country_name string `json:"country_name"`
+	// Continent_code string `json:"continent_code"`
+	// IN_eu bool `json:"in_eu"`
+	// Postal string `json:"postal"`
+	// Latitude int `json:"latitude"`
+	// Longitude int `json:"longitude"`
+	// Timezone string `json:"timezone"`
+	// UTC_offset string `json:"utc_offset"`
+	// Country_calling_code string `json:"country_calling_code"`
+	// Currency string `json:"currency"`
+	// Currency_name string `json:"currency_name"`
+	// Languages string `json:"languages"`
+	// Country_area int `json:"country_area"`
+	// Country_population int `json:"country_population"`
+	// ASN string `json:"asn"`
+	// ORG string `json:"org"`
+}
+
+/**
+ * Ascii art banner
+**/
 func showBanner() {
 	banner := `
 	_______                                                   ______                                                             
@@ -37,6 +78,10 @@ func showBanner() {
 	fmt.Println(banner)
 }
 
+/**
+ * Parameter: string url
+ * Description: Checks if a string is a url before making a request
+**/
 func isValidUrl(toTest string) bool {
 	_, err := url.ParseRequestURI(toTest)
 	if err != nil {
@@ -50,6 +95,30 @@ func isValidUrl(toTest string) bool {
 
 	return true
 }
+
+/**
+* Parameter: IP
+* Description: Makes a request to "https://ipapi.co" and parse the json result to get the
+* 			   location of the proxy
+**/
+func getlocation(ip string) string {
+	tempurl, err := http.Get("https://ipapi.co/" + ip + "/json/")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempurl.Body.Close()
+	body, _ := ioutil.ReadAll(tempurl.Body)
+	// fmt.Println(string(body))
+
+	var location Location
+	json.Unmarshal(body, &location)
+	return location.Country_name
+}
+
+func warning(msg string) {
+
+}
+
 func info(msg string) string{
 	return red + "[" + green + "+" + red + "] - " + defcol + msg
 }
@@ -58,6 +127,11 @@ func error(msg string) string {
 	return red + "[" + yellow + "!" + red + "] - " + defcol + msg
 }
 
+/** 
+* Parameter: url --> string, outfile --> pointer to a file
+* Description: Makes a proxy website and use regex to get all the IP and its port(if available)
+*			   from the site and saves them to a file
+**/
 func scraper(url string, outfile *os.File) {
 	// Split result to good proxy and bad proxy
 	resp, err := http.Get(url) 
@@ -71,6 +145,7 @@ func scraper(url string, outfile *os.File) {
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	fmt.Println("Status Code: ", resp.StatusCode)
+	// Checks the status of the page
 	if resp.StatusCode == 404 {
 		fmt.Println(error("Page doesn't exist"))
 	}else{
@@ -81,7 +156,10 @@ func scraper(url string, outfile *os.File) {
 			fmt.Printf(info("Found %d on %s\n"), len(results), url)
 			// write to a file
 			for _, proxy := range results {
-				_, err := outfile.WriteString(proxy + "\n")
+				splitProxy := strings.Split(proxy, ":")
+				location := getlocation(splitProxy[0])
+				fmt.Printf("Current proxy: %s, location: %s\n", proxy, location)
+				_, err := outfile.WriteString(proxy + "--> " + location + "\n")
 				if err != nil {
 					fmt.Println("An error occured ", err)
 					outfile.Close()
@@ -91,10 +169,39 @@ func scraper(url string, outfile *os.File) {
 	}
 }
 
+/**
+* Parameter: url --> string
+* Description: Test if the proxy works by making a request to google.com. if the request code is 200
+* 			   then we know it is valid. If otherwise, then we discard it.
+**/
 func isAlive(url string) bool {
-	return true
+	// Need to set HTTP_PROXY 
+
+	var PTransport = &http.Transport { Proxy: http.ProxyFromEnvironment }
+	client	:= http.Client { Transport: PTransport }
+
+	req, err := http.NewRequest("GET", "http://www.google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		return true
+	}
+
+	return false
 }
 
+/**
+* Description: Entry point of the program
+*/
 func main(){
 	// 1) read file from users -- DONE
 	// 2) For each line in the file(verify if it is a url) -- DONE
@@ -143,7 +250,7 @@ func main(){
 	good, _ := os.Create(*outfile)
 
 	// Banner - duh
-	// showBanner()
+	showBanner()
 
 	fmt.Println("Let's begin scraping....")
 
